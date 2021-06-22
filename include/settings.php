9bfef5df1b378ca3b7d379be1dbba128a70d093f
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-06-22 07:32:55 +0530

    Add functions for submenu

--- include/settings.php
+++ include/settings.php
@@ -1,27 +1,27 @@
 <?php
 function id_validator_settings_init(){
     register_setting(
-        "id_validator_API",
+        "id_validator_api_group",
         "id_validator_api_credentials"
         );
 
     register_setting(
-        "id_validator_API",
+        "id_validator_api_group",
         "id_validator_sheet_id"
         );
 
     register_setting(
-        "id_validator_general",
+        "id_validator_general_group",
         "id_validator_url_path"
         );
 
     register_setting(
-        "id_validator_general",
+        "id_validator_general_group",
         "id_validator_sheet_id_notation"
         );
 
     register_setting(
-        "id_validator_general",
+        "id_validator_general_group",
         "id_validator_sheet_column_notations"
         );
 
@@ -80,4 +80,34 @@ function id_validator_settings_init(){
         "id_validator_general"
         );
 
+}
+
+function id_validator_submenu(){
+    add_submenu_page(
+    "page.php",
+    "id_validator_settings_page",
+    "ID Validator",
+    "manage_options",
+    "id_validator_settings",
+    "id_validator_submenu_callback",
+);
+}
+
+function id_validator_submenu_callback() {
+    if (!current_user_can('manage_options')){
+        return;
+    }
+    ?>
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        <form action="options.php" method="post">
+            <?php
+            settings_fields('id_validator_api_group');
+            settings_fields('id_validator_general_group');
+            do_settings_sections('id_validator_settings_page');
+            submit_button( __( 'Save Settings', 'textdomain' ) );
+            ?>
+        </form>
+    </div>
+    <?php
 }
