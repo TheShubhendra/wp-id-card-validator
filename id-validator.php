6867f2acb00c7ade020676cb68c632a57f113e8d
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-06-22 06:53:14 +0530

    Add header

---
+++ id-validator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       ID Validator
+ * Description:       validates the ID Cards issued by YAIF
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Shubhendra Kushwaha
+ * Author URI:        https://github.com/TheShubhendra
+ */
+ 
+ 
