d26406d6c25725878936c26e2a9d995a228273a3
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-07-20 10:02:29 +0530

    Update version

--- id-validator.php
+++ id-validator.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       ID Validator
  * Description:       validates the ID Cards issued by YAIF
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Shubhendra Kushwaha
